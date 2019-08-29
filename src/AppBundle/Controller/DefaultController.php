0f4058697edde0bae91c8f9615a4fb4649b144bc
Marc G <mginov@gmail.com>
2019-08-29 18:17:42 +0200

    cap6 - refactor controller

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,21 +4,36 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="portada")
+     * @Route(
+     *     "/{ciudad}",
+     *     defaults={ "ciudad" = "%app.ciudad_por_defecto%" },
+     *     name="portada"
+     * )
+     * @Route("/")
      */
-    public function indexAction(Request $request)
+    public function portadaAction($ciudad)
     {
+        if (null === $ciudad) {
+            return $this->redireredirectToRoute(
+                'portada',
+                ['ciudad' => $this->getParameter('app.ciudad_por_defecto')]
+            );
+        }
+
         $em = $this->getDoctrine()->getManager();
 
         $oferta = $em->getRepository('AppBundle\Entity\Oferta')->findOneBy([
-            'ciudad' => 202,
-            //'fechaPublicacion' => new \DateTime('today'),
+            'ciudad' => $this->getParameter('app.ciudad_por_defecto'),
+            'fechaPublicacion' => new \DateTime('today'),
         ]);
+
+        if (!$oferta) {
+            throw $this->createNotFoundException('No se ha encontrado la oferta del día en la ciudad seleccionada');
+        }
 
         return $this->render('portada.html.twig', ['oferta' => $oferta]);
     }
